62e3b015600126348da2609424f3f08bcc517263
Mithilesh Ranjan <mithilesh.marssb@gmail.com>
2022-10-20 19:40:00 +0530

    Added palindrome and prime file to check

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+
+
+
+function palindrome(string){
+    let N = string.length;
+
+    for (let i=0; i<N; i++){
+        for(let j=N;j>=0;j--){
+            if(string[i]==string[j]{
+                console.log("It is a palindrome string.")
+            })else{
+                console.log("It is not a palindrome string");
+            }
+        }
+    }
+
+}
---
+++ prime.js
@@ -0,0 +1,10 @@
+
+
+
+function findPrime(num){
+   let flag = false;
+   
+   for(let i=2;i<num/2;i++){
+    
+   }
+}
