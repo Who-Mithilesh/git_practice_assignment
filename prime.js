b20b327ad4afd68a955b2426e780714d57ff33b9
ujjwalara95 <ujjwalara95@gmail.com>
2022-10-20 22:51:48 +0530

    it's done

--- prime.js
+++ prime.js
@@ -2,9 +2,20 @@
 
 
 function findPrime(num){
-   let flag = false;
-   
-   for(let i=2;i<num/2;i++){
-    
+   let flag = true;
+   if(num==1){
+      return false;
+   }else{
+      for(let i=2;i<=num/2;i++){
+         if(num%i==0){
+            flag=false;
+         }
+      }
    }
+   if(flag){
+      return true;
+   } else{
+      return false;
+   }
+   
 }
